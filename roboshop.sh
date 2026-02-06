3631a8eaa3a23d5990e8347592a9311cd6bf9a05
sgraju <sgraju.tibco@gmail.com>
2026-02-06 19:37:32 +0400

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -11,10 +11,10 @@ do
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.sgrdevsecops.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME" # sgrdevsecops.fun
     fi
         echo "$instance: $IP"
