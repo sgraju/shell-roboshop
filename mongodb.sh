0e0b576665ee7d6224a1c132169722baf72e3365
sgraju <sgraju.tibco@gmail.com>
2026-02-07 10:54:45 +0400

    shell-roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -20,10 +20,10 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
